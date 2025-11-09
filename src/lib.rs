e34a404fc66308f90ee33a3b3421ed5b93596f38
simon cardona <simon.cdna@proton.me>
2025-11-09 17:58:56 +0100

    refactor: use buffer to read file

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 pub mod config;
 
-use std::{error::Error, fs};
+use std::{error::Error, fs, io::{self, BufReader}};
 
 use config::Config;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let file = fs::read_to_string(config.file_path)?;
+    let file = fs::File::open(config.file_path)?;
+    let mut reader = BufReader::new(file);
+    let mut stdout = io::stdout();
 
-    println!("{}", file);
+    io::copy(&mut reader, &mut stdout)?;
 
     Ok(())
 }
